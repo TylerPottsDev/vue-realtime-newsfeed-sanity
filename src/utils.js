4554471ed8e92f1600b5fb988a76714eb0048165
Tyler Potts <tylerpottsdev@gmail.com>
2022-02-18 12:31:13 +0000

    Fixed Utils functions

--- src/utils.js
+++ src/utils.js
@@ -2,15 +2,10 @@ import sanity from './client'
 import imageUrlBuilder from '@sanity/image-url'
 const builder = imageUrlBuilder(sanity)
 
-const CreateURL = (source, width = 300, height = 300) => {
+export const CreateURL = (source, width = 300, height = 300) => {
 	return builder.image(source).width(width).height(height).url()
 }
 
-const TextToHTML = (text) => {
+export const TextToHTML = (text) => {
 	return text.replace(/\n/g, "<br>")
-}
-
-export default {
-	CreateURL,
-	TextToHTML
 }
